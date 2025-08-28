97aaecd3d3208cee9a44a23fe3be70e52bf0bb5b
Richard Greville <richard.greville@opticoreit.com>
2025-08-28 10:05:49 +0100

    Updated IGW tags

--- modules/inspection/internet_gateway.tf
+++ modules/inspection/internet_gateway.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
 tags = {
-  Name = "tp-${var.env}-net-${lookup(var.vpc_region_shortname, var.vpc_region)}-igw"
+  Name = "tfp-${var.env}-net-${lookup(var.vpc_region_shortname, var.vpc_region)}-igw"
   }
 }
 
