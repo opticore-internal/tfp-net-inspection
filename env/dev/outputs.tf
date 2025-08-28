c54af45e1416eaae45beed7d6e9ce0419f9c9343
Omid Mortazavi <omid_mortazavi@hotmail.com>
2025-08-28 15:22:59 +0100

    Fix subnets and ENIs

---
+++ env/dev/outputs.tf
@@ -0,0 +1,78 @@
+output "vpc_cidr_block" {
+  description = "Inspection VPC CIDR"
+  value       = module.dev-inspection.vpc_cidr_block
+}
+
+output "transit_subnet_az_0" {
+  description = "Transit subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.transit_subnet_az_0
+}
+output "transit_subnet_az_1" {
+  description = "Transit subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.transit_subnet_az_1
+}
+output "gwlbe_subnet_az_0" {
+  description = "GWLBe subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.gwlbe_subnet_az_0
+}
+output "gwlbe_subnet_az_1" {
+  description = "GWLBe subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.gwlbe_subnet_az_1
+}
+output "gwlb_subnet_az_0" {
+  description = "GWLB subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.gwlb_subnet_az_0
+}
+output "gwlb_subnet_az_1" {
+  description = "GWLB subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.gwlb_subnet_az_1
+}
+output "appliance_subnet_az_0" {
+  description = "Appliance subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.appliance_subnet_az_0
+}
+output "appliance_subnet_az_1" {
+  description = "Appliance subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.appliance_subnet_az_1
+}
+output "public_subnet_az_0" {
+  description = "Public subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.public_subnet_az_0
+}
+output "public_subnet_az_1" {
+  description = "Public subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.public_subnet_az_1
+}
+output "mgmt_subnet_az_0" {
+  description = "Mgmt subnet CIDR (AZ 0)"
+  value       = module.dev-inspection.mgmt_subnet_az_0
+}
+output "mgmt_subnet_az_1" {
+  description = "Mgmt subnet CIDR (AZ 1)"
+  value       = module.dev-inspection.mgmt_subnet_az_1
+}
+
+
+output "pafw1_mgmt_eip_address" {
+  description = "PAN1 mgmt EIP (if deployed)"
+  value       = module.dev-inspection.pafw1_mgmt_eip_address
+}
+output "pafw2_mgmt_eip_address" {
+  description = "PAN2 mgmt EIP (if deployed)"
+  value       = module.dev-inspection.pafw2_mgmt_eip_address
+}
+
+
+output "redis_primary_endpoint" {
+  description = "Redis primary endpoint (if deployed)"
+  value       = module.dev-inspection.redis_primary_endpoint
+}
+output "bootstrap_s3_bucket" {
+  description = "PAN bootstrap S3 bucket (if deployed)"
+  value       = module.dev-inspection.bootstrap_s3_bucket
+}
+
+output "bootstrap_init_cfg_s3_key" {
+  description = "init-cfg.txt S3 key (if deployed)"
+  value       = module.dev-inspection.bootstrap_init_cfg_s3_key
+}
